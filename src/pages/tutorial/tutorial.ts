e3cca3fd5bdc865c42e5e9b2499f440b113642c9
robsonklima <robsonklima@gmail.com>
2020-04-07 09:55:39 -0300

    REF

--- src/pages/tutorial/tutorial.ts
+++ src/pages/tutorial/tutorial.ts
@@ -22,28 +22,20 @@ export class TutorialPage {
     this.versao = Config.VERSAO_APP;
     this.slides = [
       {
-        title: "Deslocamento",
-        description: `Na versão <b>${this.versao}</b> você poderá informar que está em deslocamento
-                      para o local de atendimento. `,
-        image: "assets/imgs/late.png",
+        title: "Assinatura",
+        description: `Na versão <b>${this.versao}</b> você e o cliente poderão assinar o RAT. `,
+        image: "assets/imgs/form.png",
       },
       {
-        title: "Lista de Chamados",
-        description: `A tela de listagem de chamados mudou! Adicionamos os chamados fechados,
-                      e agora você poderá acompanhar os chamados que estão na lista de sincronização.`,
-        image: "assets/imgs/list.png",
+        title: "Equipamentos",
+        description: `Os equipamentos agora apresentarão apenas as causas configuradas pelo STN. `,
+        image: "assets/imgs/teller.png",
       },
       {
-        title: "Equipamentos POS",
-        description: `Esta versão contém uma nova lâmina nos detalhes do <b>chamado</b>, que deverá ser preenchida  
-                      quando você estiver atendendo um equipamento <b>POS</b>.`,
-        image: "assets/imgs/mcom.png",
-      },
-      {
-        title: "Mapas",
-        description: `Agora ao clicar no icone de <b>navegação</b> você poderá selecionar rapidamente o aplicativo
-                      de mapas que deseja utilizar (Waze, Maps etc.)`,
-        image: "assets/imgs/maps-icon-8223.png",
+        title: "Defeitos e Ações",
+        description: `Os defeitos e as ações serão apresentados de acordo com cada causa, também 
+                      configurados pela equipe de STN. `,
+        image: "assets/imgs/plugin.png",
       }
     ];
   }
