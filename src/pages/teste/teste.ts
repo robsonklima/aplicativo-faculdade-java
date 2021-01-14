b00753fe22458408f9ebc15545ded53aff2a4f00
robsonklima <robsonklima@gmail.com>
2021-01-14 18:26:18 -0300

    SIM

--- src/pages/teste/teste.ts
+++ src/pages/teste/teste.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { File } from '@ionic-native/file';
-import { Platform } from 'ionic-angular';
+import { Sim } from '@ionic-native/sim';
 
 
 @Component({
@@ -10,18 +9,22 @@ import { Platform } from 'ionic-angular';
 export class TestePage {
   
   constructor(
-    private plt: Platform,
-    private file: File
+    private sim: Sim
   ) {}
 
   ionViewWillEnter() {
-    this.plt.ready().then(() =>{
-      this.file.checkDir(this.file.externalRootDirectory, 'data').then(response => {
-      }).catch(err => {
-        this.file.createDir(this.file.externalRootDirectory, 'data', false).then(response => {
-        }).catch(err => {
-        }); 
-      });
-    });
+    this.sim.getSimInfo().then(
+      (info) => console.log('Sim info: ', info),
+      (err) => console.log('Unable to get sim info: ', err)
+    );
+    
+    this.sim.hasReadPermission().then(
+      (info) => console.log('Has permission: ', info)
+    );
+    
+    this.sim.requestReadPermission().then(
+      () => console.log('Permission granted'),
+      () => console.log('Permission denied')
+    );
   }
 }
